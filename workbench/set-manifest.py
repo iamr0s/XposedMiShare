165674319f6ba748f02fc6f4040d450ef7ca598d
iamr0s <271257581@qq.com>
2022-04-30 00:59:11 +0800

    增加分享接口

--- workbench/set-manifest.py
+++ workbench/set-manifest.py
@@ -2,13 +2,16 @@
 # -*- coding:utf-8 -*-
 
 import sys
+from tokenize import String
 import xml.dom.minidom
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        raise Exception('need input file name and output file name')
-    inputFileName = sys.argv[1]
-    outputFileName = sys.argv[2]
+    # if len(sys.argv) < 3:
+    #     raise Exception('need input file name and output file name')
+    # inputFileName = sys.argv[1]
+    # outputFileName = sys.argv[2]
+    inputFileName = 'D:\\Users\\iamr0s\\Desktops\\AndroidManifest (3).xml'
+    outputFileName = 'D:\\Users\\iamr0s\\Desktops\\AndroidManifest (2).xml'
 
     dom = xml.dom.minidom.parse(
         inputFileName)
@@ -46,8 +49,9 @@ if __name__ == '__main__':
 
     for activityNode in applicationNode.getElementsByTagName('activity'):
         activityNode.setAttribute('android:exported', 'true')
-        if activityNode.getAttribute('android:name') == 'com.miui.mishare.activity.MiShareSettingsActivity':
 
+        if activityNode.getAttribute('android:name') == 'com.miui.mishare.activity.MiShareSettingsActivity':
+            # 设置主页面
             actionNode = dom.createElement('action')
             actionNode.setAttribute(
                 'android:name', 'android.intent.action.MAIN')
@@ -61,6 +65,31 @@ if __name__ == '__main__':
             intentFilterNode.appendChild(categoryNode)
 
             activityNode.appendChild(intentFilterNode)
+        elif activityNode.getAttribute('android:name') == 'com.miui.mishare.activity.TransActivity':
+            def getSendIntentFilter(action: String):
+                actionNode = dom.createElement('action')
+                actionNode.setAttribute(
+                    'android:name', action)
+
+                categoryNode = dom.createElement('category')
+                categoryNode.setAttribute(
+                    'android:name', 'android.intent.category.DEFAULT')
+
+                dataNode = dom.createElement('data')
+                dataNode.setAttribute(
+                    'android:mimeType', '*/*')
+                dataNode.setAttribute(
+                    'android:pathPattern', '.*')
+
+                intentFilterNode = dom.createElement('intent-filter')
+                intentFilterNode.appendChild(actionNode)
+                intentFilterNode.appendChild(categoryNode)
+                intentFilterNode.appendChild(dataNode)
+                return intentFilterNode
+
+            # 设置分享页面
+            activityNode.appendChild(getSendIntentFilter('android.intent.action.SEND'))
+            activityNode.appendChild(getSendIntentFilter('android.intent.action.SEND_MULTIPLE'))
 
     file = open(outputFileName, "w")
     file.write(dom.toprettyxml(indent=' '))
